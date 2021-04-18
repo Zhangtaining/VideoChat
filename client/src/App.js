43167d1a0466a9b5697d095e12517792bd8fd33d
zcao1208 <caozhongli1208@gmail.com>
2021-04-18 02:45:07 -0400

    sign page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import SignInComponent from './component/SignIn.component';
+import SignUpComponent from './component/SignUp.component';
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Meeting App
-        </a>
-      </header>
-    </div>
-  );
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>VideoChat</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SignInComponent} />
+            <Route path="/sign-in" component={SignInComponent} />
+            <Route path="/sign-up" component={SignUpComponent} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>);
 }
 
 export default App;
