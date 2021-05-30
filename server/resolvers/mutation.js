b17d578e50aa33c53de5243a428b85dd46245451
zcao1208 <caozhongli1208@gmail.com>
2021-05-30 09:50:52 -0400

    fix login logic

--- server/resolvers/mutation.js
+++ server/resolvers/mutation.js
@@ -1,6 +1,6 @@
 var { hash, compare } = require('bcrypt');
 var { sign } = require('jsonwebtoken');
-import con from '../db_utils/db-connection'
+import { Database } from '../db_utils/db-connection'
 const APP_SECRET = 'Video_Chart_V1';
 const saltRounds = 10;
 
@@ -19,29 +19,29 @@ async function signup(name, password, email) {
     }
 }
 
-async function login(name, password) {
+function login(name, password) {
     const valid = true // await compare(password, user.password)
     //console.log(con+ "555555")
-    return con.user_login(name, password).then((res, err) => {
-        //console.log(res);
-        // console.log(user_info)
-        if (!valid) {
-            throw new Error('Invalid password')
-        }
-
-        const user = {
-            id: user_info.user_id,
-            name: name,
-            email: 'test@test.com',
-        }
-
-        const token = sign({userId: user.id}, APP_SECRET)
-
-        return {
-            token,
-            user
-        }
-    }) 
+    console.log("I'm here >_< ^O^")
+    const query = `SELECT user_id, user_name, email_address FROM USER WHERE user_name = '${user_name}' AND password = '${password}'`;
+    const db = new Database();
+    console.log("I'm here >_< ^O^")
+    return db.query(query).then( rows => {
+        console.log(rows);
+        return new Promise((resolve, reject) => {
+            const user = {
+                id: user_info.user_id,
+                name: name,
+                email: 'test@test.com',
+            }
+    
+            const token = sign({userId: user.id}, APP_SECRET)
+            resolve({
+                token,
+                user
+            });
+        })
+    });
 }
 
 export default {
